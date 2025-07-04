8e75da1a5fff98ba2c0ca0ef730573ba3eccb2a4
Lucas Barros <lucasweacked21@gmail.com>
2025-07-04 01:09:15 -0300

    init: inicio do projeto ECHO

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="flex items-center h-16 bg-[var(--color-preto-profundo)] px-5">
+      <h1 className="font-[family-name:var(--font-playfair)] text-[var(color-branco-lavado)] text-3xl italic font-regular">
+        ECHO
+      </h1>
+    </header>
+  );
+}
